ca9cd158eef9b1819064466cbe6e212306ca7b05
LkRonald <rpereirac@unicartagena.co>
2025-11-03 17:00:33 -0500

    feat: se agrego la clase Main

---
+++ Ejercicio1/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Figura circulo = new Circulo(3);
+        Figura rectangulo = new Rectangulo(5,5);
+
+        circulo.calcularArea();
+        rectangulo.calcularArea();
+
+    }
+}
