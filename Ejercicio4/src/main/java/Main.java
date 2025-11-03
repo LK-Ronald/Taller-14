66dedc9f9100a587c44d2da1c638ac3c0a9e4c00
LkRonald <rpereirac@unicartagena.co>
2025-11-03 17:52:57 -0500

    feat: se agrego la clase Main

---
+++ Ejercicio4/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Vehiculo coche  = new Coche();
+        Vehiculo bicicleta = new Bicicleta();
+
+        coche.mover();
+        bicicleta.mover();
+    }
+}
