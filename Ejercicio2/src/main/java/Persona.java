846b3c8c69c5cfe02d7327ae008c47123fbc7a68
LkRonald <rpereirac@unicartagena.co>
2025-11-03 17:28:29 -0500

    feat: se agrago la clase Persona

---
+++ Ejercicio2/src/main/java/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    protected String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void presentarse(){
+        System.out.println("Hola soy nuevo en la escuela");
+    }
+}
